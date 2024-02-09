8d5149b1048572555c7fe6f9e50352012d561d38
archana-adhi <aadhi@mail.yu.edu>
2024-02-09 03:49:39 -0500

    init_commit

--- aim5005/features.py
+++ aim5005/features.py
@@ -31,7 +31,8 @@ class MinMaxScaler:
         diff_max_min = self.maximum - self.minimum
         
         # TODO: There is a bug here... Look carefully! 
-        return x-self.minimum/(self.maximum-self.minimum)
+        #The bug is to put the numerator value of return statement in brackets
+        return (x-self.minimum) /(self.maximum-self.minimum)
     
     def fit_transform(self, x:list) -> np.ndarray:
         x = self._check_is_array(x)
@@ -42,4 +43,30 @@ class MinMaxScaler:
 class StandardScaler:
     def __init__(self):
         self.mean = None
-        raise NotImplementedError
+        self.std = None
+        # raise NotImplementedError
+    
+    def _check_is_array(self, x:np.ndarray) -> np.ndarray:
+        if not isinstance(x, np.ndarray):
+            x = np.array(x)
+            
+        assert isinstance(x, np.ndarray), "Expected the input to be a list"
+        return x
+    
+    def fit(self, X) -> None:
+        self.mean = np.mean(X, axis=0)
+        self.std = np.std(X, axis=0)
+        
+    def transform(self, X):
+        if self.mean is None or self.std is None:
+            raise Exception("Scaler has not been fitted yet")
+        return (X - self.mean) / self.std
+    
+    def fit_transform(self, X):
+        self.fit(X)
+        return self.transform(X)
+    
+    def inverse_transform(self,X_scaled):
+        if self.mean is None or self.std is None:
+            raise Exception("Scaler has not been fitted yet")
+        return X_scaled * self.std + self.mean
--- tests/test_features.py
+++ tests/test_features.py
@@ -51,6 +51,7 @@ class TestFeatures(TestCase):
         data = [[0, 0], [0, 0], [1, 1], [1, 1]]
         expected = np.array([[-1., -1.], [-1., -1.], [1., 1.], [1., 1.]])
         scaler.fit(data)
+        result = scaler.transform(data)
         assert (result == expected).all(), "Scaler transform does not return expected values. Expect {}. Got: {}".format(expected.reshape(1,-1), result.reshape(1,-1))
         
     def test_standard_scaler_single_value(self):
@@ -62,6 +63,17 @@ class TestFeatures(TestCase):
         assert (result == expected).all(), "Scaler transform does not return expected values. Expect {}. Got: {}".format(expected.reshape(1,-1), result.reshape(1,-1))
 
     # TODO: Add a test of your own below this line
+    #Custom test case    
+        
+    def test_standard_scaler_inverse_transform(self):
+        scaler = StandardScaler()
+        data = [[0, 0], [0, 0], [1, 1], [1, 1]]
+        scaler.fit(data)
+        transformed_data = scaler.transform(data)
+        # Perform inverse transform on the transformed data
+        inverse_transformed_data = scaler.inverse_transform(transformed_data)
+        assert (data == inverse_transformed_data).all(), "Inverse transform did not return the original data. Got: {}".format(inverse_transformed_data)
+  
     
 if __name__ == '__main__':
     unittest.main()
